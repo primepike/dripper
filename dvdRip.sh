6124e5931067e9adffbac6c600bd8063de00be43
primepike <mike.p.piper@gmail.com>
2013-08-18 20:27:46 +0100

    update logging for dvdRip script

--- dvdRip.sh
+++ dvdRip.sh
@@ -5,16 +5,16 @@
 # Usage: dvdRip.sh <device>
 #
 
-DRIPPER_DIR=/home/mike/dripper
+DRIPPER_DIR=$HOME/dripper
 OUTPUT_DIR=$HOME/dvd-rips/copy
 DEVICE=$1
 
-echo "DVD $DEVICE inserted" >> dvd.log
+echo "DVD $DEVICE inserted" >> $DRIPPER_DIR/logs/dvd.log
 
 TITLE=`$DRIPPER_DIR/python/getDVDTitle $DEVICE`
 DVDLABEL=`$DRIPPER_DIR/python/getDVDLabel $DEVICE`
 
-echo "$TITLE mounted" >> dvd.log
+echo "$TITLE mounted" >> $DRIPPER_DIR/logs/dvd.log
 
 dvdbackup -M -i $DEVICE -o $OUTPUT_DIR
 
