d58e97d9e5109d602a99e66404c6bf3771b3994b
primepike <mike.p.piper@gmail.com>
2013-08-18 19:46:27 +0100

    dvdRip script & add extra python scripts

--- dvdRip.sh
+++ dvdRip.sh
@@ -5,8 +5,17 @@
 # Usage: dvdRip.sh <device>
 #
 
+DRIPPER_DIR=/home/mike/dripper
+OUTPUT_DIR=$HOME/dvd-rips/copy
 DEVICE=$1
 
 echo "DVD $DEVICE inserted" >> dvd.log
 
+TITLE=`$DRIPPER_DIR/python/getDVDTitle $DEVICE`
+DVDLABEL=`$DRIPPER_DIR/python/getDVDLabel $DEVICE`
 
+echo "$TITLE mounted" >> dvd.log
+
+dvdbackup -M -i $DEVICE -o $HOME/dvds
+
+echo "$TITLE" > $OUTPUT_DIR/$DVDLABEL/discName.txt
