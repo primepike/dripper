136428a7a69954f5112ca3533e0a26c884d45708
primepike <mike.p.piper@gmail.com>
2013-08-19 22:54:45 +0100

    improved dvdbackup output, needs directing to right folder(is broken)

--- dvdRip.sh
+++ dvdRip.sh
@@ -21,9 +21,16 @@ echo "$TITLE mounted" >> $DRIPPER_DIR/logs/dvd.log
 
 #check if already ripped here
 
-dvdbackup -M -i $DEVICE -o $OUTPUT_DIR
+#---
 
-echo "$TITLE" > $OUTPUT_DIR/$DVDLABEL/discName.txt
+CONSOLE_LOG_FILE=$DRIPPER_DIR/logs/$DVDLABEL-copylog.txt
+
+dvdbackup -M -p -v -I -i $DEVICE -o $OUTPUT_DIR > $CONSOLE_LOG_FILE 2>&1
+
+MOUNTPOINT="$DRIPPER_DIR/python/getDVDMountPoint $DEVICE"
+DETAILS_FILE="$OUTPUT_DIR/$DVDLABEL/Disc-Details.txt"
+echo "dvd-title=$TITLE" >> $DETAILS_FILE
+cp $CONSOLE_LOG_FILE $OUTPUT_DIR/$DVDLABEL/
 
 echo "ejecting disk: $DEVICE"
 umount $DEVICE
